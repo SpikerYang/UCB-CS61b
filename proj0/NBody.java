94adb7e277aa0768c678b43e8c02763ea1148312
spike <61228498@qq,com>
2019-02-04 01:08:39 +0800

    proj0 completed!

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,55 @@
 public class NBody {
+	public static double readRadius(String file){
+		In i = new In(file);
+		i.readDouble();
+		return i.readDouble();
+	}
 
+	public static Planet[] readPlanets(String file){
+		In i = new In(file);
+		i.readDouble();
+		i.readDouble();
+		Planet[] p = new Planet[5];
+		for (int k = 0; k < 5; k += 1){
+			p[k] = new Planet(i.readDouble(), i.readDouble(), i.readDouble(), i.readDouble(), i.readDouble(), i.readString());
+		}
+		return p;
+	}
+
+	public static void main(String args[]){
+		double T = Double.valueOf(args[0]);
+		double dt = Double.valueOf(args[1]);
+		String filename = args[2];
+		double Radius = readRadius(filename);
+		Planet p[] = readPlanets(filename);   /* Collecting all needed input */
+                
+		StdAudio.play("./audio/2001.mid");
+		StdDraw.setScale(-Radius, Radius);
+                        StdDraw.clear();
+                        StdDraw.picture(0, 0, "./images/starfield.jpg", 2*Radius, 2*Radius);
+                        for (int k = 0; k < p.length; k += 1){
+                                p[k].draw();
+                        }
+                        StdDraw.show(10);
+
+
+		for (double time = 0; time < T; time += dt){
+			double[] xForces = new double[p.length];
+			double[] yForces = new double[p.length];
+			for (int k = 0; k < p.length; k += 1){
+				xForces[k] = p[k].calcNetForceExertedByX(p);
+				yForces[k] = p[k].calcNetForceExertedByY(p);
+			}
+			for (int k = 0; k <p.length; k += 1){
+				p[k].update(dt, xForces[k], yForces[k]);
+			}
+			StdDraw.setScale(-Radius, Radius);
+			StdDraw.clear();
+			StdDraw.picture(0, 0, "./images/starfield.jpg", 2*Radius, 2*Radius);
+               		for (int k = 0; k < p.length; k += 1){
+				p[k].draw();
+			}
+			StdDraw.show(10);
+		}
+	}
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,66 @@
 public class Planet {
+	public double xxPos; /* Its current x position */
+	public double yyPos; /* Its current y position */
+	public double xxVel; /* Its current velocity in the x direction */
+	public double yyVel; /* Its current velocity in the y direction */
+	public double mass; /* Its mass */
+	public String imgFileName; /* The name of an image in the images directory that depicts the planet */
+        
+	public Planet(double xP, double yP, double xV, double yV, double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+	
+	public Planet(Planet p){
+		xxPos = p.xxPos;
+                yyPos = p.yyPos;
+                xxVel = p.xxVel;
+                yyVel = p.yyVel;
+                mass = p.mass;
+                imgFileName = p.imgFileName;
+	}
+
+	public double calcDistance(Planet p){
+		return Math.sqrt(Math.pow((xxPos-p.xxPos),2) + Math.pow((yyPos-p.yyPos),2));
+	}
+	public double calcForceExertedBy(Planet p){
+		double d = calcDistance(p);
+		return 6.67e-11 * mass * p.mass /d /d;
+	}
+	public double calcForceExertedByX(Planet p){
+		return calcForceExertedBy(p) * (p.xxPos - xxPos) / calcDistance(p);
+	}
+	public double calcForceExertedByY(Planet p){
+		return calcForceExertedBy(p) * (p.yyPos - yyPos) / calcDistance(p);
+	}
+	public double calcNetForceExertedByX(Planet[] p){
+		double F = 0;
+		for(int i = 0; i < p.length; i += 1){
+			if (!this.equals(p[i])) F += calcForceExertedByX(p[i]);
+		}
+                return F;
+	}
+        public double calcNetForceExertedByY(Planet[] p){
+                double F = 0;
+                for(int i = 0; i < p.length; i += 1){
+                        if (!this.equals(p[i])) F += calcForceExertedByY(p[i]);
+                }
+                return F;
+        }
+	public void update(double dt, double fX, double fY){
+		double aX = fX / mass;
+		double aY = fY / mass;
+		xxVel += aX * dt;
+		yyVel += aY * dt;
+		xxPos += xxVel * dt;
+		yyPos += yyVel * dt;
+	}
+	public void draw(){
+		StdDraw.picture(xxPos, yyPos, "./images/" + imgFileName);
+	}
+
 }
